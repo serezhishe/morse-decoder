d0c6fc1d0464deaf291407a2014408bf002196b3
serezhishe <serezhishe@gmail.com>
2019-10-01 15:38:48 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -38,7 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const morse_letters = [];
+    const lat_letters = [];
+    expr.split('**********')
+        .map(el => el.split(''))
+        .forEach(element => {
+        while (element.length) morse_letters.push(element.splice(0, 10))
+        morse_letters.push(' ');
+        });
+    morse_letters.pop();
+    morse_letters.forEach(el => {
+        const kek = []
+        if (el === ' ') lat_letters.push([[' ']])
+        else { while (el.length) {
+              kek.push(el.splice(0, 2))
+            }
+            lat_letters.push(kek);
+        }
+    })
+    return lat_letters.map(element => element.map(el => {
+        if (el.join('') === '00') return '';
+        if (el.join('') === '10') return '.';
+        if (el.join('') === '11') return '-';
+     })).map(el => MORSE_TABLE[el.join('')])
+        .map(el => el ? el : ' ').join('');
 }
 
 module.exports = {
